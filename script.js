7e7772a2750c204b02a0959730836448f4a0af9d
Raj Roy <royr938559@gmail.com>
2025-07-31 00:03:38 +0530

    implementation of cursor effect

--- script.js
+++ script.js
@@ -3,42 +3,58 @@ let navBar = document.querySelector("nav");
 let navLinks = document.querySelector("nav ul");
 let darkModeButton = document.querySelector("#darkButton");
 
-
-function openMenu(){
-    sideMenu.style.transform = 'translateX(-16rem)';
-    
+function openMenu() {
+  sideMenu.style.transform = "translateX(-16rem)";
 }
-function closeMenu(){
-    sideMenu.style.transform = 'translateX(16rem)';
-    
+function closeMenu() {
+  sideMenu.style.transform = "translateX(16rem)";
 }
 
-window.addEventListener("scroll", ()=>{
-    if(scrollY > 50){
-        navBar.classList.add( 'bg-[#fffff0]','backdrop-blur-lg', 'shadow-sm', 'transition', 'duration-400',);
-        navLinks.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50');
-    } else{
-        navBar.classList.remove("bg-white", 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
-        navLinks.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50');
-    }
+window.addEventListener("scroll", () => {
+  if (scrollY > 50) {
+    navBar.classList.add(
+      "bg-[#fffff0]",
+      "backdrop-blur-lg",
+      "shadow-sm",
+      "transition",
+      "duration-400"
+    );
+    navLinks.classList.remove("bg-white", "shadow-sm", "bg-opacity-50");
+  } else {
+    navBar.classList.remove(
+      "bg-white",
+      "bg-opacity-50",
+      "backdrop-blur-lg",
+      "shadow-sm"
+    );
+    navLinks.classList.add("bg-white", "shadow-sm", "bg-opacity-50");
+  }
 });
 
+///////////////
+// Scroll Reveal Animation
+///////////////
+const animate = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: "2500",
+  delay: "400",
+});
 
+animate.reveal(".nav");
+animate.reveal(".animation-left", { origin: "left" });
+animate.reveal(".animation-bottom", { origin: "bottom" });
+animate.reveal(".animation-right", { origin: "right" });
+animate.reveal(".boxes", { intervel: 100 });
 
+const cursor = document.querySelector("#cursor");
+const body = document.querySelector("body");
 
-  ///////////////
-  // Scroll Reveal Animation
-  ///////////////
-  const animate = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: '2500',
-    delay: '400',
-
+body.addEventListener("mousemove", (debt) => {
+  gsap.to(cursor, {
+    x: debt.x,
+    y: debt.y,
+    ease: "back.out",
+    duration: 1,
   });
-
-  animate.reveal(".nav");
-  animate.reveal(".animation-left", {origin: "left"});
-  animate.reveal(".animation-bottom", {origin: "bottom"});
-  animate.reveal(".animation-right",{origin : "right"});
-  animate.reveal(".boxes", {intervel: 100});
+});
